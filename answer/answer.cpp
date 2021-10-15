ccb36ccef9aff1105b7ded82428483e23e791963
Richard Chien <stdrc@outlook.com>
2021-10-15 22:20:19 +0800

    use cache string to allow configurable APPID

--- answer/answer.cpp
+++ answer/answer.cpp
@@ -12,7 +12,7 @@ namespace answer {
     namespace v2 {
         std::string find_the_ultimate_answer() {
             // 改成了调用 wolfram 库的 API，而不是使用 CURL 发请求
-            return wolfram::simple_query("YAPKJY-8XT9VEYPX9", "what is the ultimate answer?");
+            return wolfram::simple_query(WOLFRAM_APPID, "what is the ultimate answer?");
         }
     } // namespace v2
 } // namespace answer
