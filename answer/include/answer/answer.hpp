2b914f3daf6ef75cc63c342f7b23a92f2cdd30e6
Richard Chien <stdrc@outlook.com>
2021-10-15 22:33:27 +0800

    make libanswer a header-only lib

--- answer/include/answer/answer.hpp
+++ answer/include/answer/answer.hpp
@@ -2,13 +2,21 @@
 
 #include <string>
 
+#include <wolfram/alpha.hpp>
+
+// header-only 库的所有实现代码均在头文件中
+
 namespace answer {
     namespace v1 {
-        int find_the_ultimate_answer();
+        int find_the_ultimate_answer() {
+            return 42;
+        }
     } // namespace v1
 
     namespace v2 {
-        std::string find_the_ultimate_answer();
+        std::string find_the_ultimate_answer() {
+            return wolfram::simple_query(WOLFRAM_APPID, "what is the ultimate answer?");
+        }
     } // namespace v2
 
     using namespace v2;
